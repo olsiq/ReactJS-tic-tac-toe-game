6d9913595be013d3972f126d3537129d871ecf93
olsiq <64708189+olsiq@users.noreply.github.com>
2022-02-01 12:54:02 +0200

    fix minor stuff

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { App } from './app/App.js';
-// import { Game } from './components';
+import { App } from './app';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {/* <Game /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-const ab = '1213';
