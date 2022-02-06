b8544459b71a1180e01f6b49ecfe2e1e026fc18a
olsiq <64708189+olsiq@users.noreply.github.com>
2022-02-06 20:39:50 +0200

    delete old reducers

--- src/libraries/reducers/historyReducer.js
+++
@@ -1,8 +0,0 @@
-const historyReducer = (state, action) => {
-  //console.log('history action works');
-  if (action.type === 'CHANGE_HISTORY') {
-    return (state = action.payload);
-  }
-};
-
-export default historyReducer;
--- src/libraries/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import playerReducer from './playerReducer';
-import stepReducer from './stepReducer';
-import historyReducer from './historyReducer';
-import oneReducer from './oneReducer';
-
-export { playerReducer, stepReducer, historyReducer, oneReducer };
