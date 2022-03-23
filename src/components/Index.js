97ea81c7396030689ded2731d5bbc16fdf36cdef
olsiq <64708189+olsiq@users.noreply.github.com>
2022-03-23 17:24:59 +0200

    import/export bug

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,5 +1,5 @@
-export { Game } from "./game";
-export { Board } from "./board";
-export { Square } from "./square";
+import { Game } from "./game";
+import { Board } from "./board";
+import { Square } from "./square";
 
-// export { Game, Board, Square };
+export { Game, Board, Square };
